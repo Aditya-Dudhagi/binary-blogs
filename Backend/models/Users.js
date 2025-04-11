afc81af321bbb52a888fa10a86f71f6fcfac2ea2
Aditya-Dudhagi <adityadudhagi28@gmail.com>
2025-04-11 15:55:46 +0530

    Add User model with schema validation

--- Backend/models/Users.js
+++ Backend/models/Users.js
@@ -1,1 +1,35 @@
 // schema for user
+
+// import mongoose
+const mongoose = require("mongoose");
+
+// define the user schema
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true, // each username should be different
+      trim: true, // removes extra spaces
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true, // makes all emails lowercase to avoid duplicates
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true, // auto-creates `createdAt` and `updatedAt`
+  });
+
+// Create the model from the schema
+const User = mongoose.model('User', userSchema)
+
+// export it
+module.exports = User
