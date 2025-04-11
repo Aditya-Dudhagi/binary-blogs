230f4d820054296b0ac72d6dbd8c069316fb4a27
Aditya-Dudhagi <adityadudhagi28@gmail.com>
2025-04-11 17:07:44 +0530

    Add register route with schema validation and password hashing

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,1 +1,38 @@
-const User = require('../models/Users')
+const express = require("express");
+const router = express.Router();
+
+const bcrypt = require('bcryptjs')
+const User = require("../models/Users");
+
+
+// building the /register route
+
+router.post('/register',async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        const existingUser = await User.findOne({email})
+        if (existingUser) {
+            return res.status(400).json({message: "User already exists"})
+        }
+
+        // Hash the password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        // create and save new user
+        const newUser = new User({
+            username, 
+            email,
+            password: hashedPassword,
+        })
+        
+        await newUser.save();
+
+        res.status(201).json({message: 'User registered successfully'})
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error')
+    }
+})
+
+module.exports = router;
